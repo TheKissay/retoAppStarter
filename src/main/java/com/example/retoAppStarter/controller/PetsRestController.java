3babe964ce610a950fb4b5e1375b00f7f1f3d1a3
TheKissay <thekissay112@gmail.com>
2021-10-20 22:34:55 +0200

    Añadido PetsRestController

--- src/main/java/com/example/retoAppStarter/controller/PetsRestController.java
+++ src/main/java/com/example/retoAppStarter/controller/PetsRestController.java
@@ -1,15 +1,25 @@
 package com.example.retoAppStarter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pets_library.config.PetsProperties;
 
 @RestController
 @RequestMapping("/pets")
 public class PetsRestController {
 
 	
-	@GetMapping
+	@Autowired PetsProperties s;
+	
+	
+	@GetMapping("/saludo")
+	public String getSaludo() {
+		System.out.println(s.getLenguaje());
+		return s.getLenguaje();
+	}
 	public String showPets() {
 		return "Lista de mascotas";
 	}
