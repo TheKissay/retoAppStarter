7c4fb5dcdb4dfe69bef7969394d111c2082cf088
TheKissay <thekissay112@gmail.com>
2021-10-20 14:27:16 +0200

    Initial commit

---
+++ src/main/java/com/example/retoAppStarter/RetoAppStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.retoAppStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoAppStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoAppStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/retoAppStarter/RetoAppStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retoAppStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoAppStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
